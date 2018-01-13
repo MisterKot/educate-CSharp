9bf5da29c882f64abe5b31752ccd30193dfd3fef
RomanTask <roman260887@gmail.com>
2018-01-13 22:02:08 +0500

    сделал первое и второе задание лабораторной

---
+++ Lab1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+            Console.Write("Введите первое число: ");
+            String num = Console.ReadLine();
+            int a = Int32.Parse(num);
+            Console.Write("Введите второе число: ");
+            num = Console.ReadLine();
+            int b= Int32.Parse(num);
+            Console.Write("Введите третье число: ");
+            num = Console.ReadLine();
+            int c = Int32.Parse(num);
+            int y = a * b * c;
+            Console.WriteLine($"Произведение чисел: {a}*{b}*{c}={y}");
+            Console.WriteLine($"Обратный порядок: {c}*{b}*{a}={y}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab1/ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое трехзначное число: ");
+            String num = Console.ReadLine();
+            
+            int [] nums = new int[num.Length];
+            for (int i = 0; i<num.Length; i++)
+            {
+                nums[i] = num[i];
+            }
+
+            int compos = 1; //произведение
+
+            foreach (int a in nums)
+            {
+                compos *= a;
+            }
+            Console.WriteLine($"Произведение цифр данного числа = {compos}");
+            Console.ReadKey();
+     
+        }
+    }
+}
